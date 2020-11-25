fa1542bc0099faf45e906f4a1bf3886e1c0afcd6
Vladii <vladii@Vladiis-MacBook-Air.local>
2020-11-25 22:42:32 +0300

    test

--- sc.js
+++
@@ -1,8 +0,0 @@
-menu.onclick = function bar(){
-    var x = document.getElementById("Mytopnav");
-    if (x.className === "topnav"){
-        x.className+=" responsive";
-    } else {
-        x.className="topnav";
-    }
-}
